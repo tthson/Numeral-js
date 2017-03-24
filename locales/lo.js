48b76a97085ce2e2cf1d3fbf22df6487e9c14866
tranthaison <tthsonvn@gmail.com>
2017-03-24 11:52:57 +0700

    add locale for Vietnamese Lao and Cambodia

---
+++ locales/lo.js
@@ -0,0 +1,32 @@
+// numeral.js locale configuration
+// locale : Lao (lo)
+// author : Tran Thai Son : https://github.com/tthsonvn
+
+(function (global, factory) {
+    if (typeof define === 'function' && define.amd) {
+        define(['../numeral'], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        factory(require('../numeral'));
+    } else {
+        factory(global.numeral);
+    }
+}(this, function (numeral) {
+    numeral.register('locale', 'lo', {
+        delimiters: {
+            thousands: '.',
+            decimal: ','
+        },
+        abbreviations: {
+            thousand: 'k',
+            million: 'm',
+            billion: 'b',
+            trillion: 't'
+        },
+        ordinal : function (number) {
+            return '';
+        },
+        currency: {
+            symbol: '₭'
+        }
+    });
+}));
